73bbdf09e704f0a49f9f0872fbf38586f1065fca
jfspps <jfsapps@gmail.com>
2020-07-15 18:01:12 +0100

    refresh build

---
+++ target/classes/data.sql
@@ -0,0 +1,14 @@
+-- the tables category and unit_of_measure are not defined anywhere-else in the project
+-- the columns description relate to the String description local variables in Category and UnitOfMeasure
+INSERT INTO category (description) VALUES ('American');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('Mexican');
+INSERT INTO category (description) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (description) VALUES ('Each');
+INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (description) VALUES ('Cup');
+INSERT INTO unit_of_measure (description) VALUES ('Pinch');
+INSERT INTO unit_of_measure (description) VALUES ('Ounce');
+INSERT INTO unit_of_measure (description) VALUES ('Dash');
+INSERT INTO unit_of_measure (description) VALUES ('Pint');
