167221caa33bbf87822e189558825f739c20362d
jfspps <jfsapps@gmail.com>
2020-07-15 17:16:06 +0100

    added Mockito ArgumentCaptor example

--- src/test/java/com/example/recipe/controllers/indexControllerTest.java
+++ src/test/java/com/example/recipe/controllers/indexControllerTest.java
@@ -1,17 +1,21 @@
 package com.example.recipe.controllers;
 
+import com.example.recipe.model.Recipe;
 import com.example.recipe.services.RecipeServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class indexControllerTest {
 
@@ -52,5 +56,44 @@ public class indexControllerTest {
         //equals matcher eq() to check that modelString is "recipes"; anySet() is a generic Set for testing purposes
         verify(model, times(1)).addAttribute(eq(modelString), anySet());
 
+    }
+
+    @Test
+    public void getIndexPage_arguments() {
+        //use the given, when, then for behaviour testing
+        /**
+                (Given) some context
+                (When) some action is carried out
+                (Then) a particular set of observable consequences should obtain
+        */
+
+        String modelString = "recipes";
+
+        //given
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1L);
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(2L);
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(recipe1);
+        recipes.add(recipe2);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        // ArgumentCaptor<T> is a set which stores arguments passed between calls related to object T
+        // the new instance pertains specifically to Set methods
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
+        String returnString = indexController.getIndexPage(model);
+
+
+        //then
+        assertEquals("index", returnString);
+        //capture the arguments passed between Model.addAttribute
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        //pull argument data across to a set and check if two
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
