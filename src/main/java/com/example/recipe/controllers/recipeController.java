657b523b584c45daa1536af0c54946f487fbfefa
jfspps <jfsapps@gmail.com>
2020-07-19 21:29:10 +0100

    added new Recipe form and POST request routing

--- src/main/java/com/example/recipe/controllers/recipeController.java
+++ src/main/java/com/example/recipe/controllers/recipeController.java
@@ -1,9 +1,12 @@
 package com.example.recipe.controllers;
 
+import com.example.recipe.commands.RecipeCommand;
 import com.example.recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +23,24 @@ public class recipeController {
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    //posted form accessed through recipe/new will create a blank RecipeCommand object
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    //see recipeform.html for /recipe/ POST request component
+    //@ModelAttribute takes the model provided from newRecipe()
+    //Recall that saveRecipeCommand converts command to POJO and back
+    //return the id of the savedCommand and redirect to the show.html page
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
