fa8340f3dfbd30ee094dd74b245b5640e7522eac
jfspps <jfsapps@gmail.com>
2020-07-20 18:01:08 +0100

    added IngredientService and tests

--- src/main/java/com/example/recipe/services/IngredientServiceImpl.java
+++ src/main/java/com/example/recipe/services/IngredientServiceImpl.java
@@ -1,11 +1,15 @@
 package com.example.recipe.services;
 
 import com.example.recipe.commands.IngredientCommand;
+import com.example.recipe.converters.IngredientCommandToIngredient;
 import com.example.recipe.converters.IngredientToIngredientCommand;
+import com.example.recipe.model.Ingredient;
 import com.example.recipe.model.Recipe;
 import com.example.recipe.repositories.RecipeRepository;
+import com.example.recipe.repositories.UnitOfMeasureRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -14,16 +18,23 @@ import java.util.Optional;
 public class IngredientServiceImpl implements IngredientService {
 
     private final IngredientToIngredientCommand ingredientToIngredientCommand;
+    private final IngredientCommandToIngredient ingredientCommandToIngredient;
     private final RecipeRepository recipeRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
 
-    public IngredientServiceImpl(IngredientToIngredientCommand ingredientToIngredientCommand, RecipeRepository recipeRepository) {
+    public IngredientServiceImpl(IngredientToIngredientCommand ingredientToIngredientCommand,
+                                 IngredientCommandToIngredient ingredientCommandToIngredient,
+                                 RecipeRepository recipeRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
         this.ingredientToIngredientCommand = ingredientToIngredientCommand;
+        this.ingredientCommandToIngredient = ingredientCommandToIngredient;
         this.recipeRepository = recipeRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
     }
 
     @Override
     public IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId) {
 
+        //query the recipe first before the ingredient
         Optional<Recipe> recipeOptional = recipeRepository.findById(recipeId);
 
         if (!recipeOptional.isPresent()){
@@ -33,15 +44,75 @@ public class IngredientServiceImpl implements IngredientService {
 
         Recipe recipe = recipeOptional.get();
 
+        //match the captured recipe with the ingredient with i ingredientId, and then convert to IngredientCommand
         Optional<IngredientCommand> ingredientCommandOptional = recipe.getIngredients().stream()
                 .filter(ingredient -> ingredient.getId().equals(ingredientId))
                 .map(ingredientToIngredientCommand::convert).findFirst();
 
+        //if we can't find ingredient then this'll need handling in future
         if(!ingredientCommandOptional.isPresent()){
             //todo impl error handling
             log.error("Ingredient id not found: " + ingredientId);
         }
 
         return ingredientCommandOptional.get();
+    }
+
+    //verify all commands are performed before committing to the DB
+    @Override
+    @Transactional
+    public IngredientCommand saveIngredientCommand(IngredientCommand command) {
+        // Overall strategy is build a Recipe (parent) POJO, save it to the DB (or update if it exists)
+        // and then return the IngredientCommand property. Thus associated properties persist.
+
+        // make sure that the recipe property of ingredient is initialised, then extract
+        Optional<Recipe> recipeOptional = recipeRepository.findById(command.getRecipeId());
+
+        if(!recipeOptional.isPresent()){
+
+            //todo toss error if not found!
+            log.error("Recipe not found for id: " + command.getRecipeId());
+            // save() will not return null but will not save anything to the DB if recipe property is missing
+            return new IngredientCommand();
+        } else {
+            // convert Optional<Recipe> to Recipe and extract all ingredients then pull out ingredient by ID
+            Recipe recipe = recipeOptional.get();
+
+            // this approach verifies a recipe has been associated with the IngredientCommand
+            Optional<Ingredient> ingredientOptional = recipe
+                    .getIngredients()
+                    .stream()
+                    .filter(ingredient -> ingredient.getId().equals(command.getId()))
+                    .findFirst();
+
+            if(ingredientOptional.isPresent()){
+                // the ingredient ingredient has two nested properties: Recipe (handled above) and UnitOfMeasure
+                Ingredient ingredientFound = ingredientOptional.get();
+
+                //build temporary Ingredient POJO from IngredientCommand
+                ingredientFound.setDescription(command.getDescription());
+                ingredientFound.setAmount(command.getAmount());
+
+                //extract and apply the UOM
+                ingredientFound.setUom(unitOfMeasureRepository
+                        .findById(command.getUom().getId())
+                        .orElseThrow(() -> new RuntimeException("UOM NOT FOUND"))); //todo address this
+            } else {
+                //this adds command's ingredient POJO without verification of UOM (might be empty)
+                recipe.addIngredient(ingredientCommandToIngredient.convert(command));
+            }
+
+            //save Recipe and all changes to each associated ingredient to the DB
+            Recipe savedRecipe = recipeRepository.save(recipe);
+
+            //to do check for fail
+            return ingredientToIngredientCommand.convert(savedRecipe
+                    .getIngredients()
+                    .stream()
+                    .filter(recipeIngredients -> recipeIngredients.getId().equals(command.getId()))
+                    .findFirst()
+                    .get());
+        }
+
     }
 }
